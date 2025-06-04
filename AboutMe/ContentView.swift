3c6061114bd2f04acad7f034b14a79b03947e757
Ana <anasingh197@gmail.com>
2025-06-04 13:58:25 -0500

    couldn't tell u

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,31 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Color(red: 0.20, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: 0.70)
-            Text("Ana Singh")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(Color.blue)
-            //my name
-            Divider()
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(Color.blue)
-            Image("ana")
-                .resizable(capInsets: EdgeInsets(top: 0.0, leading: -10.0, bottom: 0.0, trailing: -10.0), resizingMode: .stretch)
-                .aspectRatio(contentMode: .fill)
-            //picture
-            Divider()
-            Text("Hi! I'm a incoming junior at Marquette High School. I play lacrosse and field hockey & I have an older sister!")
-            
-                .foregroundColor(Color.blue)
-            //bottom text
-            Color(red: 0.20, green: 0.5, blue: 0.70)
-            LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Color(red: 0.20, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: 0.70)
+                Text("Ana Singh")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.blue)
+                //my name
+                Divider()
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.blue)
+                Image("ana")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: -10.0, bottom: 0.0, trailing: -10.0), resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                //picture
+                Divider()
+                Text("Hi! I'm a incoming junior at Marquette High School. I play lacrosse and field hockey & I have an older sister!")
+                    .font(.headline)
+                
+                    .foregroundColor(Color.blue)
+                //bottom text
+                Color(red: 0.40, green: 0.5, blue: 0.70)
+                Spacer()
+                Text("I love 7Brew!!!")
+                    .font(.body)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.blue)
+                    .frame(width: 300, height: 200)
+                .border(Color.blue)
+                Image("yay")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            }
+            //end stack
         }
-        //end stack
-        
     }
     //end body
 }
