ed04ab37f1beab080659238db7151e70234af038
Ana <anasingh197@gmail.com>
2025-06-04 13:27:57 -0500

    added image and text

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("ana")
-            .resizable(resizingMode: .stretch)
-            .aspectRatio(contentMode: .fit)
+        
+        VStack {
+            Text("Ana Singh")
+            //my name
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.blue)
+            Image("ana")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: -10.0, bottom: 0.0, trailing: -10.0), resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+            //picture
+            Text("Hi")
+            //bottom text
+        }
+        //end stack
+        
     }
     //end body
 }
